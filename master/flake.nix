3c9b292b129252ae912757c7f44a6c15af1619ad
hugosenari <hugosenari@gmail.com>
2022-03-12 00:59:03 -0300

    chore: re index cache

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A cache library.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-cache-master.flake = false;
+  inputs.src-cache-master.owner = "planety";
+  inputs.src-cache-master.ref   = "refs/heads/master";
+  inputs.src-cache-master.repo  = "cached";
+  inputs.src-cache-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-cache-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-cache-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
